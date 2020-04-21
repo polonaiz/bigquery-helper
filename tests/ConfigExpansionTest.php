62ff9a61edd2502acb54edcf3558ce55613ef8fe
polonaiz <polonaiz@gmail.com>
2020-04-21 13:58:57 +0900

    refine test code

--- tests/ConfigExpansionTest.php
+++ tests/ConfigExpansionTest.php
@@ -7,23 +7,31 @@ class ConfigExpansionTest extends TestCase
 {
 	public function testConfiguration()
 	{
-		$accessControlConfiguration =
-			\json_decode(\file_get_contents(
-				__DIR__ . "/Asset/AccessControl/sample1-givenAccessControlConfiguration.json"
-			), true);
-		$datasetIds =
-			\json_decode(\file_get_contents(
-				__DIR__ . "/Asset/AccessControl/sample1-givenDatasetIds.json"
-			), true);
+		foreach ([[
+			'accessControlConfiguration' => __DIR__ . "/Asset/AccessControl/sample1-givenAccessControlConfiguration.json",
+			'datasetIds' => __DIR__ . "/Asset/AccessControl/sample1-givenDatasetIds.json",
+			'expectedAccessControl' => __DIR__ . "/Asset/AccessControl/sample1-expectedAccessControl.json"
+		], [
+			'accessControlConfiguration' => __DIR__ . "/Asset/AccessControl/sample2-givenAccessControlConfiguration.json",
+			'datasetIds' => __DIR__ . "/Asset/AccessControl/sample2-givenDatasetIds.json",
+			'expectedAccessControl' => __DIR__ . "/Asset/AccessControl/sample2-expectedAccessControl.json"
+		], [
+			'accessControlConfiguration' => __DIR__ . "/Asset/AccessControl/sample3-givenAccessControlConfiguration.json",
+			'datasetIds' => __DIR__ . "/Asset/AccessControl/sample3-givenDatasetIds.json",
+			'expectedAccessControl' => __DIR__ . "/Asset/AccessControl/sample3-expectedAccessControl.json"
+		]] as $testset)
+		{
+			$accessControlConfiguration = \json_decode(\file_get_contents($testset['accessControlConfiguration']), true);
+			$datasetIds = \json_decode(\file_get_contents($testset['datasetIds']), true);
 
-		$result =
-			$this->expandConfig($accessControlConfiguration, $datasetIds);
+			$result =
+				$this->expandConfig($accessControlConfiguration, $datasetIds);
 
-		$this->assertEquals(
-			\json_encode(\json_decode(\file_get_contents(
-				__DIR__ . "/Asset/AccessControl/sample1-expectedAccessControl.json"), true), JSON_PRETTY_PRINT),
-			\json_encode(
-				$result, JSON_PRETTY_PRINT));
+			$this->assertEquals(
+				\json_encode(\json_decode(\file_get_contents(
+					$testset['expectedAccessControl']), true), JSON_PRETTY_PRINT),
+				\json_encode($result, JSON_PRETTY_PRINT));
+		}
 	}
 
 	/**
@@ -67,9 +75,103 @@ class ConfigExpansionTest extends TestCase
 						$datasetAccessDict[$datasetId]['access'][] = ['role' => $accessEntry['role']] + $member;
 					}
 				}
+				unset($accessEntry);
+
+				(new ArrayHandler($datasetAccessDict[$datasetId]['access']))
+					->sort([$this, 'accessEntryComparator'])
+					->uniq([$this, 'accessEntryComparator']);
 			}
 		}
 
 		return \array_values($datasetAccessDict);
+	}
+
+	public function accessEntryComparator($accessEntry1, $accessEntry2)
+	{
+		return \strcmp(\json_encode($accessEntry1), \json_encode($accessEntry2));
+	}
+
+	public function testArrayUnique()
+	{
+		//
+		$data = [
+			["role" => "READER", "userByEmail" => "tester-004@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-001@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-001@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-002@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-003@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-002@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-003@gmail.com"],
+			["role" => "READER", "userByEmail" => "tester-001@gmail.com"],
+		];
+
+		(new ArrayHandler($data))
+			->sort([$this, 'accessEntryComparator'])
+			->uniq([$this, 'accessEntryComparator']);
+
+		//
+		$this->assertEquals(
+			\json_encode([
+				["role" => "READER", "userByEmail" => "tester-001@gmail.com"],
+				["role" => "READER", "userByEmail" => "tester-002@gmail.com"],
+				["role" => "READER", "userByEmail" => "tester-003@gmail.com"],
+				["role" => "READER", "userByEmail" => "tester-004@gmail.com"],
+			]),
+			\json_encode($data));
+	}
+
+	public function testArrayHandle()
+	{
+		//
+		$data = [2, 4, 3, 1, 2, 4];
+
+		//
+		$intComparator = fn($v1, $v2) => $v1 === $v2 ? 0 : ($v1 < $v2 ? -1 : +1);
+		(new ArrayHandler($data))
+			->sort($intComparator)
+			->uniq($intComparator);
+
+		//
+		$this->assertEquals(
+			\json_encode([1, 2, 3, 4]),
+			\json_encode($data)
+		);
+	}
+}
+
+class ArrayHandler
+{
+	private array $data;
+
+	public function __construct(array &$data)
+	{
+		$this->data = &$data;
+	}
+
+	public function sort(callable $comparer)
+	{
+		\usort($this->data, $comparer);
+
+		return $this;
+	}
+
+	public function uniq(callable $comparer)
+	{
+		$carry = \array_reduce(
+			$this->data,
+			function ($carry, $current) use (&$previous, $comparer)
+			{
+				if ($comparer($previous, $current) !== 0)
+				{
+					$carry[] = $current;
+				}
+				$previous = $current;
+				return $carry;
+			},
+			$initialCarry = []
+		);
+		$this->data = $carry;
+
+		return $this;
 	}
 }
